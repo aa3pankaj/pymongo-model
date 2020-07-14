b6c4aae58f1ddc5654c5ee87c438d74919795204
aa3pankaj <aa3pankaj@gmail.com>
2020-07-15 02:35:03 +0530

    readme update

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ REQUIREMENTS = ['jsondiff','pymongo']
 
 setuptools.setup(
     name="pymongo-model",
-    version="1.0.5",
+    version="1.0.6",
     author="Pankaj Singh",
     author_email="aa3pankaj@gmail.com",
-    description="Simple model based usage of pymongo, also provides models for mongoDB document history tracking",
+    description="Simple model based usage of pymongo, also provides optional feature for tracking mongoDB document history",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/aa3pankaj/pymongo-model",
